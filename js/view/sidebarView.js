1075882feaab654ccc7ce3b1f5c1ba933276521a
Eli-Ionescu <ionescu.e.f@gamil.com>
2019-01-28 14:00:19 +0100

    update sidebarView

--- js/view/sidebarView.js
+++ js/view/sidebarView.js
@@ -7,8 +7,8 @@
 var SidebarView = function (container, model) {
 
     // Selection menu for number of people
-    var numberPeopleSelect = container.find("#numberPeople");
-    var numberPeople = model.getNumberOfGuests();
+    let numberPeopleSelect = container.find("#numberPeople");
+    let numberPeople = model.getNumberOfGuests();
     let selectPeople = "";
     for (let i=1; i <= numberPeople; i++){
         selectPeople += "<option>";
@@ -18,22 +18,22 @@ var SidebarView = function (container, model) {
     numberPeopleSelect.html(selectPeople);
 
     // Get a list with selected dishes and add them in the sidebar table
-    var selectedDish = container.find("#selectedDishTableBody");
-    var allSelectedDishes = model.getAllSelectedDishes();
-    var selectedDishHTML = "";
+    let selectedDish = container.find("#selectedDishTableBody");
+    let allSelectedDishes = model.getAllSelectedDishes();
+    let selectedDishHTML = "";
 
     for(let dish in allSelectedDishes){
         selectedDishHTML += "<tr>\n";
         selectedDishHTML += "<td scope=\"row\">" + dish.name + "</td>\n";
-        selectedDishHTML += " <td>"+ model.getDishPrice(dish.id) +"</td>\n";
+        selectedDishHTML += "<td>"+ model.getDishPrice(dish.id) +"</td>\n";
         selectedDishHTML += "</tr>";
     }
     selectedDish.html(selectedDishHTML);
 
     // Get the total price of the menu
-    var totalPrice = container.find("#totalPrice");
-    var totalPriceValue = model.getTotalMenuPrice();
-    var totalPriceHTML = "Total: ";
+    let totalPrice = container.find("#totalPrice");
+    let totalPriceValue = model.getTotalMenuPrice();
+    let totalPriceHTML = "Total: ";
     totalPriceHTML += totalPriceValue.toString();
     totalPriceHTML += "SEK";
     totalPrice.html(totalPriceHTML);
