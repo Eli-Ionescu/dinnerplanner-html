12c598419e47d8a26dfa1999e5e4cac44489b0ba
Ruxandra22 <ruxandra22.osman@gmail.com>
2019-02-07 17:14:41 +0100

    added the ingredient list to the dish details view

--- js/view/dishDetailsView.js
+++ js/view/dishDetailsView.js
@@ -6,9 +6,17 @@ class DishDetailsView extends GeneralView{
         this.model = model;
         this.id = id ? id : model.getCurrentId();
 
+        this.ingredients = [];
+        // this.getRecepiesSearch()
+        //     .then(data => {
+        //         this.dishes = data.results;
+        //         this.baseUri = data.baseUri;
+        //     })
+        //     .catch(error => console.error(error));
         this.addDynamicElements();
         this.button = document.getElementById("buttonAddToMenu");
         this.backButton = document.getElementById("buttonBackToSearch");
+
     }
 
     updateDescription (dish) {
@@ -16,8 +24,32 @@ class DishDetailsView extends GeneralView{
       elemenet.innerHTML = dish["summary"];
     }
 
-    updateIngredients(ingredients) {
-        console.log(ingredients);
+    updateIngredients(ingredients, totalIngredientsPrice) {
+        let dishDetailIngredients = this.container.querySelector("#dishIngredients");
+        let nrPeople = this.model.getNumberOfGuests();
+        let dishIngredients = `<h3>Ingredients for ${nrPeople} people</h3>
+                        <table class="table">
+                        <tbody>`;
+
+        this.ingredients.forEach(ingredient => {
+            console.log(ingredient);
+            let totalQuantity = ingredient.amount * nrPeople;
+            // let totalPrice = ingredients[i].price * nrPeople;
+            dishIngredients += `<tr>
+                        <td scope="row"> ${totalQuantity} ${ingredient.unit} </td>
+                        <td>${ingredient.name}</td>
+                        <!--<td> SEK </td>-->
+                        <!--<td> bla</td>-->
+                    </tr>`;
+        });
+        dishIngredients += `</tbody>
+                    </table>`;
+
+        // Compute price depending on how many people are
+        let totalPrice = totalIngredientsPrice * nrPeople;
+        dishIngredients += `<p align="right">Total: ${totalPrice} SEK </p>
+                </div>`;
+        dishDetailIngredients.innerHTML = dishIngredients;
     }
 
     addDynamicElements () {
@@ -26,7 +58,6 @@ class DishDetailsView extends GeneralView{
         let dishID = this.model.getCurrentId();
         let dish = this.model.getDish(dishID);
 
-        console.log(dishID);
         let dishDescription = `<h3 id="dishNameID">${dish.title}</h3>
                         <div class="row" id="imageDetails">
                             <img id="imageDetailsId" class="img-thumbnail" src="${this.model.baseUri}${dish.image}" alt="${dish.title}">
@@ -40,53 +71,14 @@ class DishDetailsView extends GeneralView{
           .catch( error => {});
 
         this.model.getDishIngredients(dishID)
-            .then(ingredients => this.updateIngredients(ingredients))
+            .then(data => {
+                this.ingredients = data[0].extendedIngredients;
+                this.totalIngredientsPrice = data[0].pricePerServing;
+                this.updateIngredients(this.ingredients, this.totalIngredientsPrice);
+            })
             .catch(error => {});
 
-
         $(".se-pre-con").fadeOut("slow");
-
-        // update title and image path
-
-        // let dishDescription = `<h3 id="dishNameID">${dish.name}</h3>
-        //                     <div class="row" id="imageDetails">
-        //                         <img class="img-thumbnail" src="images/${dish.image}" alt=${dish.name}>
-        //                     </div>
-        //                     <p> ${dish.description}</p>`;
-
-
-
-        // Ingredients list
-        // let dishDetailIngredients = this.container.querySelector("#dishIngredients");
-        // let ingredients = dish.ingredients;
-        // let nrPeople = this.model.getNumberOfGuests();
-        //
-        // let dishIngredients = `<h3>Ingredients for ${nrPeople} people</h3>
-        //                 <table class="table">
-        //                 <tbody>`;
-        //
-        // for (let i in ingredients) {
-        //     let totalQuantity = ingredients[i].quantity * nrPeople;
-        //     let totalPrice = ingredients[i].price * nrPeople;
-        //     dishIngredients += `<tr>
-        //                     <td scope="row"> ${totalQuantity} ${ingredients[i].unit} </td>
-        //                     <td>${ingredients[i].name}</td>
-        //                     <td> SEK </td>
-        //                     <td> ${totalPrice}</td>
-        //                 </tr>`;
-        // }
-        //
-        // dishIngredients += `</tbody>
-        //             </table>`;
-        //
-        // // Compute total
-        // let dishPrice = this.model.getDishPrice(dish.id);
-        // // the total price depending on how many people are
-        // let totalPrice = dishPrice * nrPeople;
-        // dishIngredients += `<p align="right">Total: ${totalPrice} SEK </p>
-        //             </div>`;
-        //
-        // dishDetailIngredients.innerHTML = dishIngredients;
     }
 
     update(model, changeDetails) {
